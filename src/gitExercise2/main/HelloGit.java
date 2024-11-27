5072dc9e8f2caea9d4fa1f2309413344cba7b68d
m.zanzi1@campus.unimib.it <m.zanzi1@campus.unimib.it>
2024-11-27 12:29:17 +0100

    sono il primo dev e modifico riga

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -4,6 +4,6 @@ public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
 		System.out.println("Accipicchia, stampa sovrascritta, ed ancora modificato");
-		System.out.println("Hello git!!! MODIFICATA");
+		System.out.println("Accipicchia, stampa sovrascritta, ed ancora modificato");
 	}
 }
